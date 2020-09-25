432339cf7270a4111463ea5c4e03663127d10740
RomanTomal <57354716+romantomal@users.noreply.github.com>
2020-09-25 14:45:05 +0300

    Back local axios

--- yellow-project-front/src/theme/axios.js
+++ yellow-project-front/src/theme/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://yellow-test-project.herokuapp.com/",
+    baseURL: "http://localhost:8080",
     responseType: "json"
 });
